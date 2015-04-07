391db1d9c815d1b80b2e04235ba5e0e125e48227
Marco Carrizales <marco_carrizales@msn.com>
2015-04-07 12:28:01 -0500

    Almost done
    Only divide the image array and we are ready to go.

--- app/src/main/java/com/example/sweg/hook/imageSender.java
+++ app/src/main/java/com/example/sweg/hook/imageSender.java
@@ -2,7 +2,6 @@ package com.example.sweg.hook;
 
 import android.os.AsyncTask;
 import android.util.Log;
-
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -13,12 +12,14 @@ public class ImageSender {
     int port;
     byte[] fileBytes;
     byte[] fileName;
+    String bytesLength;
 
     public ImageSender(String ip, String port, byte[] bytes, String fileName) {
         this.ip = ip;
         this.port = Integer.parseInt(port);
         this.fileBytes = bytes;
         this.fileName = fileName.getBytes();
+        this.bytesLength = "" + fileBytes.length;
     }
 
     public void run() {
@@ -37,11 +38,30 @@ public class ImageSender {
                 Log.d("UDP", "C: Connecting...");
                         /* Create new UDP-Socket */
                 DatagramSocket socket = new DatagramSocket();
-                DatagramPacket packetDirection = new DatagramPacket(fileBytes,
-                        fileBytes.length, serverAddress, port);
-                /* Send out the packet */
+                DatagramPacket packetDirection;
+
+                for(int i = 0; i < 3; i ++){
+                    switch(i){
+                        case 0:
+                            //Sends the length of the byte array of the image
+                            packetDirection = new DatagramPacket(bytesLength.getBytes(),
+                                    bytesLength.getBytes().length, serverAddress, port);
+                            break;
+                        case 1:
+                            //Sends the name of the image file
+                            packetDirection = new DatagramPacket(fileName,
+                                    fileName.length, serverAddress, port);
+                            break;
+                        default:
+                            //Sends the image
+                            packetDirection = new DatagramPacket(fileBytes,
+                                    fileBytes.length, serverAddress, port);
+                            break;
+                    }
+                    /* Send out the packet */
                     socket.send(packetDirection);
-                Log.d("UDP", "C: Sending");
+                    Log.d("UDP", "C: Sending");
+                }
             } catch (Exception e) {
                 Log.e("UDP", "C: Error", e);
             }
